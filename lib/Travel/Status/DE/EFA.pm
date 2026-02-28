7c51655e7968b00e5b830864d0758f5b69983970
Birte Kristina Friesel <derf@finalrewind.org>
2026-02-28 19:59:42 +0100

    Update copyright

--- lib/Travel/Status/DE/EFA.pm
+++ lib/Travel/Status/DE/EFA.pm
@@ -979,7 +979,7 @@ efa-m(1), Travel::Status::DE::EFA::Departure(3pm).
 
 =head1 AUTHOR
 
-Copyright (C) 2011-2025 Birte Kristina Friesel E<lt>derf@finalrewind.orgE<gt>
+Copyright (C) 2011-2026 Birte Kristina Friesel E<lt>derf@finalrewind.orgE<gt>
 
 =head1 LICENSE
 
